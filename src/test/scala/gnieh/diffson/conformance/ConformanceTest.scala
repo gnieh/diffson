26a6c9d6c9c01575064c2eb260413d94c974e7d4
Lucas Satabin <lucas.satabin@esterel-technologies.com>
2015-03-23 19:16:24 +0100

    Add support for conformance tests

---
+++ src/test/scala/gnieh/diffson/conformance/ConformanceTest.scala
@@ -0,0 +1,20 @@
+package gnieh.diffson
+package conformance
+
+import spray.json._
+
+sealed trait ConformanceTest
+
+case class SuccessConformanceTest(doc: JsValue,
+  patch: JsArray,
+  expected: Option[JsValue],
+  comment: Option[String],
+  disabled: Option[Boolean]) extends ConformanceTest
+
+case class ErrorConformanceTest(doc: JsValue,
+  patch: JsArray,
+  error: String,
+  comment: Option[String],
+  disabled: Option[Boolean]) extends ConformanceTest
+
+case class CommentConformanceTest(comment: String) extends ConformanceTest
