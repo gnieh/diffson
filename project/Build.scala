5310b8ed6a95092e9e53fde4d14a94b80478d0f9
Lucas Satabin <lucas.satabin@gnieh.org>
2016-02-08 12:41:28 +0100

    Prepare for next version

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object DiffsonBuild extends Build {
     base = file(".")) settings (
     organization := "org.gnieh",
     name := "diffson",
-    version := "1.1.0",
+    version := "1.2.0-SNAPSHOT",
     scalaVersion := "2.11.7",
     description := "Json diff/patch library",
     licenses += ("The Apache Software License, Version 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0.txt")),
