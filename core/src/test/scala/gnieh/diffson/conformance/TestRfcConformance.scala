2256cd2dd58b34b6acbebe5b8fff3538e21105bb
Lucas Satabin <lucas.satabin@gnieh.org>
2018-03-08 18:59:37 +0100

    Formatting

--- core/src/test/scala/gnieh/diffson/conformance/TestRfcConformance.scala
+++ core/src/test/scala/gnieh/diffson/conformance/TestRfcConformance.scala
@@ -28,18 +28,18 @@ abstract class TestRfcConformance[JsValue, Instance <: DiffsonInstance[JsValue]]
   trait ConformanceTest
 
   case class SuccessConformanceTest(
-    doc: JsValue,
-    patch: JsArray,
-    expected: Option[JsValue],
-    comment: Option[String],
-    disabled: Option[Boolean]) extends ConformanceTest
+      doc: JsValue,
+      patch: JsArray,
+      expected: Option[JsValue],
+      comment: Option[String],
+      disabled: Option[Boolean]) extends ConformanceTest
 
   case class ErrorConformanceTest(
-    doc: JsValue,
-    patch: JsArray,
-    error: String,
-    comment: Option[String],
-    disabled: Option[Boolean]) extends ConformanceTest
+      doc: JsValue,
+      patch: JsArray,
+      error: String,
+      comment: Option[String],
+      disabled: Option[Boolean]) extends ConformanceTest
 
   case class CommentConformanceTest(comment: String) extends ConformanceTest
 
